65685ac72806d0f913e57887d091f37ff34bbf49
=sayantan-git1 <=sayantanchoudhury@instaveritas.com>
2022-09-14 16:33:25 +0530

    Implemented ngIf-Else, ngSwitch & ngFor

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgIfElseComponent } from './ng-if-else/ng-if-else.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfElseComponent,
+    NgSwitchComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/src/app/ng-if-else/ng-if-else.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-if-else',
+  templateUrl: './ng-if-else.component.html',
+  styleUrls: ['./ng-if-else.component.css']
+})
+export class NgIfElseComponent implements OnInit {
+
+  constructor() { }
+  
+  color="green"
+
+  ngOnInit(): void {
+  }
+
+}
